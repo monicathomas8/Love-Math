a150f051ecdb8cc99723fa53ffb7584a061fac97
Monica Thomas <monicathomas8@icloud.com>
2024-10-10 16:12:35 +0000

    created rungame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
-function runGame() {}
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+
+function runGame() {
+    
+    // creates 2 randpm numbers
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
 
 function checkAnswer() {}
 
